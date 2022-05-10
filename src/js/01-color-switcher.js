346960fc14b99b1d2def6fb2ad65a7e9f3a3116f
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-05-10 10:50:14 +0300

    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+// const refs = {
+//   start: document.querySelector(''),
+// }
+const start = document.querySelector('[data-start]'),
+stop = document.querySelector('[data-stop]');
+start.addEventListener('click', startColor);
+stop.addEventListener('click', stopColor);
 
+
+let dataId = 0;
+function startColor() { 
+renderColor(); 
+start.disabled = true;
+stop.disabled = false;
+
+}
+
+function renderColor() {
+  const colorBody = () => document.body.style.backgroundColor = getRandomHexColor();
+  dataId = setInterval(colorBody, 1000);
+}
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+
+ function stopColor() {
+  clearInterval(dataId);
+  start.disabled = false;
+  stop.disabled = true;
+}
+//  renderColor();
